2d610edb30536cf0d49817d0a9e97c0aff78832d
nagadomi <nagadomi@nurs.or.jp>
2017-03-28 20:09:16 +0900

    Fix sample.rb when no faces were detected #2

--- animeface-ruby/sample.rb
+++ animeface-ruby/sample.rb
@@ -10,13 +10,12 @@ image = Magick::ImageList.new(ARGV[0])
 output = File.basename(ARGV[0]).split(".").first + "_out.png"
 faces = AnimeFace::detect(image)
 # when you want to change parameter
-# faces = AnimeFace::detect(image, {:step => 2.0, :min_window_size => 24, :threshold => 0.1})
+#faces = AnimeFace::detect(image, {:step => 2.0, :min_window_size => 24, :threshold => 0.1})
 pp faces
 
 gc = Magick::Draw.new
 gc.stroke = 'red'
 gc.fill = 'transparent'
-
 faces.each do |ctx|
   face = ctx["face"]
   left_eye = ctx["eyes"]["left"]
@@ -45,9 +44,14 @@ faces.each do |ctx|
   score_gc.pointsize = 16
   score_gc.annotate(image, 0, 0, face["x"], [face["y"]- 2, 0].max, sprintf("%.3f", ctx["likelihood"]))
 end
-gc.draw(image)
-image.write(output)
-puts "See #{output}"
+unless faces.empty?
+  gc.draw(image)
+  image.write(output)
+  puts "#{faces.size} faces"
+  puts "See #{output}"
+else
+  warn "No faces were detected."
+end
 
 #result = AnimeFace::detect(image, { :step => 2.0, :min_window_size => 32, :scale_factor => 1.1 })
 #pp result
